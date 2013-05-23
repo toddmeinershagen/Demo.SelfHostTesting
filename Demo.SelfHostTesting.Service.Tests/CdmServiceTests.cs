8ee7a68806c3a34a9373d22827c0685927bacf2e
Todd Meinershagen <todd@meinershagen.net>
2013-05-23 10:28:49 -0500

    Added logic to test for complex type return and negotiation based on Accept header.

--- Demo.SelfHostTesting.Service.Tests/CdmServiceTests.cs
+++ Demo.SelfHostTesting.Service.Tests/CdmServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using NUnit.Framework;
 using Nancy;
@@ -10,29 +11,48 @@ namespace Demo.SelfHostTesting.Service.Tests
     [TestFixture]
     public class CdmServiceTests
     {
-        private readonly NancyHost _host = new NancyHost(new Uri("http://localhost:1234"));
+        private const string Uri = "http://localhost:1234";
+        private readonly NancyHost _host = new NancyHost(new Uri(Uri));
+        private IRestClient _client;
 
         [TestFixtureSetUp]
         public void Init()
         {
             _host.Start();
-        }
-
-        [Test]
-        public void given_single_cdm_exists_when_providing_procedure_code_should_return_cdm()
-        {
-            var client = new RestClient {BaseUrl = "http://localhost:1234"};
-
-            var request = new RestRequest("api/v/1/facilities/1234/cdms");
-            var result = client.Get(request);
-
-            result.Content.Should().Be("Hello World from Version:  1, Facility:  1234");
+            _client = new RestClient { BaseUrl = Uri };
         }
 
         [TestFixtureTearDown]
         public void Dispose()
         {
             _host.Stop();
+        }
+
+        [Test]
+        public void given_facility_1234_when_providing_procedure_code_should_return_message_with_params()
+        { 
+            var request = new RestRequest("api/v/1/facilities/1234/cdms");
+            var result = _client.Get(request);
+
+            result.Content.Should().Be("Hello World from Version:  1, Facility:  1234");
+        }
+
+        [Test]
+        public void given_facility_2345_when_providing_procedure_code_should_return_matching_cdm()
+        {
+            var request = new RestRequest("api/v/1/facilities/2345/cdms?ProcedureCodes[]=ABC123");
+            var result = _client.Get(request);
+
+            result.Content.Should().Be("[{\"ProcedureCode\":\"ABC123\",\"ChargeCode\":\"C123\",\"RevenueCode\":\"R1234\",\"PerUnitCharge\":1.25}]");
+        }
+
+        [Test]
+        public void given_facility_does_not_exist_when_providing_procedure_code_should_return_404_status_code()
+        {
+            var request = new RestRequest("api/v/1/facilities/12/cdms");
+            var result = _client.Get(request);
+
+            result.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
         }
     }
 
@@ -42,12 +62,40 @@ namespace Demo.SelfHostTesting.Service.Tests
         {
             Get["api/v/{version}/facilities/{facilityId}/cdms"] = parameters =>
                 {
+
                     if (parameters.facilityId == "1234")
                         return string.Format("Hello World from Version:  {0}, Facility:  {1}", parameters.version,
                                              parameters.facilityId);
-                    else
-                        return HttpStatusCode.NotFound;
+
+                    if (parameters.facilityId == "2345")
+                        return
+                            Negotiate.WithModel(new List<ChargeDescriptionMaster>
+                                {
+                                    new ChargeDescriptionMaster
+                                        {
+                                            ChargeCode = "C123",
+                                            PerUnitCharge = 1.25m,
+                                            ProcedureCode = Context.Request.Query["ProcedureCodes[]"],
+                                            RevenueCode = "R1234"
+                                        }
+                                });
+
+                    return HttpStatusCode.NotFound;
                 };
+        }
+
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        public class ChargeDescriptionMaster
+        {
+            public string ProcedureCode { get; set; }
+            public string ChargeCode { get; set; }
+            public string RevenueCode { get; set; }
+            public decimal PerUnitCharge { get; set; }
         }
     }
 }
